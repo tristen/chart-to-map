698387d2b7427465a35763827558f0a7474f6d51
tristen <tristen.brown@gmail.com>
2018-09-26 20:33:14 -0400

    Update from a style layer back to an xd doc
    Ref #4

--- bin/index.js
+++ bin/index.js
@@ -10,17 +10,19 @@ const tmp = require('tmp');
 const fs = require('fs');
 const { merge, flattenDeep } = require('lodash');
 
+const { parseName } = require('../lib/parse-name');
 const { manifestParser } = require('../lib/manifest-parser');
 const { artboardToStyleLayers } = require('../lib/artboard-to-style-layers');
+const { mapElementToStyleLayer } = require('../lib/map-element-to-style-layer');
 
 const cli = meow(`
   ${chalk.bold('Usage')}
-    $ ${commandName} --from <input> --to <input> 
- 
+    $ ${commandName} --from <input> --to <input>
+
   ${chalk.bold('Options')}
     --from, -f  Provide a from input. This is where changes will be read from. Must be a Adobe XD file or Mapbox style template.
     --to, -t  Provide a to input. This is where changes will updated on. Must be a Adobe XD file or Mapbox style template.
- 
+
   ${chalk.bold('Examples')}
     $ ${commandName} --from foo.xd --to foo.json
     $ ${commandName} --from foo.json --to foo.xd
@@ -72,26 +74,68 @@ function xdOutput(err) {
 
   const manifest = manifestParser(directory);
 
-  const layersDerivedFromXD = flattenDeep(manifest.artboards.map(d => {
-    const json = fs.readFileSync(`${directory.name}/artwork/${d.path}/graphics/graphicContent.agc`, 'utf-8');
-    const artboard = JSON.parse(json);
-    return artboard.children.map(artboardToStyleLayers);
-  }));
+  if (fileExtension(from) === 'xd') {
+    // We are reading from an XD file and weriting to a style template.
 
-  const newLayers = style.layers.map(layer => {
-    const { id } = layer;
-    const found = layersDerivedFromXD.find(d => d.id === id);
+    const layersDerivedFromXD = flattenDeep(manifest.artboards.map(d => {
+      const json = fs.readFileSync(`${directory.name}/artwork/${d.path}/graphics/graphicContent.agc`, 'utf-8');
+      const collection = JSON.parse(json);
+      return collection.children.map(artboardToStyleLayers);
+    }));
 
-    if (found) {
-      return merge(layer, found);
-    } else {
-      return layer;
-    }
-  }); 
+    const newLayers = style.layers.map(layer => {
+      const { id } = layer;
+      const found = layersDerivedFromXD.find(d => d.id === id);
 
-  style.layers = newLayers;
+      if (found) {
+        return merge(layer, found);
+      } else {
+        return layer;
+      }
+    });
 
-  fs.writeFile(`${stripExtension(xdFile)}.json`, JSON.stringify(style, null, 2), error => {
-    if (error) throw new Error(error);
-  });
+    style.layers = newLayers;
+
+    fs.writeFile(`${stripExtension(xdFile)}.json`, JSON.stringify(style, null, 2), error => {
+      if (error) throw new Error(error);
+    });
+  } else if (fileExtension(from) === 'json') {
+
+    // 1. Locate each artboard.
+    // 2. Within each child element locate its equivalent style layer
+    // 3. Update that child element
+    // 4. Write the finished result back to the .agc file.
+
+    flattenDeep(manifest.artboards.map(d => {
+      const path = `${directory.name}/artwork/${d.path}/graphics/graphicContent.agc`;
+      const collection = JSON.parse(fs.readFileSync(path, 'utf-8'));
+
+      collection.children = collection.children.map(c => {
+        return c.artboard.children.map(element => {
+          const { name, valid } = parseName(element);
+
+          if (valid) {
+            // Retrieve the equivalent style layer for this artboard element.
+            const matchingLayer = style.layers.find(l => l.id === name);
+
+            if (!matchingLayer) {
+              return element;
+            } else {
+              return mapElementToStyleLayer(element, matchingLayer);
+            }
+          }
+
+          return element;
+        });
+      });
+
+      fs.writeFile(path, JSON.stringify(collection, null, 2), error => {
+        if (error) throw new Error(error);
+      });
+    }));
+
+  } else {
+    console.log('A valid --from input must be provided.');
+    cli.showHelp();
+  }
 }
